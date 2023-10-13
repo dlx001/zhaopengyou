dfac373903bccc330d7b14c125441fdee3f78b49
daniel-xu-silk <140107537+daniel-xu-silk@users.noreply.github.com>
2023-10-13 11:07:49 -0700

    initial server and client setup

---
+++ server/index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+const PORT = 4000;
+
+const http = require("http").Server(app);
+const cors = require("cors");
+
+app.use(cors());
+
+const socketIO = require("socket.io")(http, {
+  cors: {
+    origin: "http://localhost:3001",
+  },
+});
+
+socketIO.on("connection", (socket) => {
+  console.log(` ${socket.id} user just connected!`);
+  socket.on("disconnect", () => {
+    console.log("A user disconnected");
+  });
+});
+
+app.get("/api", (req, res) => {
+  res.json({
+    message: "Hello world",
+  });
+});
+
+http.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
